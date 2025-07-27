3aeda0675c9fa5ae57837d04c29234a7b10727d2
Elephant47 <vandinhdung2003@gmail.com>
2025-07-27 17:20:16 +0700

    update

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -3,4 +3,4 @@ aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS
 docker pull 324037310518.dkr.ecr.ap-southeast-1.amazonaws.com/animatem:latest
 docker stop animatem-container || true
 docker rm animatem-container || true
-docker run -d --name animatem-container -p 3000:3000 <account-id>.dkr.ecr.<region>.amazonaws.com/animatem:latest
+docker run -d --name animatem-container -p 3000:3000 324037310518.dkr.ecr.ap-southeast-1.amazonaws.com/animatem:latest
