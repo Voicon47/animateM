ce20b9c30dadf1f2368205ce22f12d98c8664996
Elephant47 <vandinhdung2003@gmail.com>
2025-07-29 11:19:46 +0700

    Auto delete old image

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 324037310518.dkr.ecr.ap-southeast-1.amazonaws.com
+docker image prune -f
 docker pull 324037310518.dkr.ecr.ap-southeast-1.amazonaws.com/animatem:latest
 docker stop animatem-container || true
 docker rm animatem-container || true
